0fb228a083605614600706ee9553e40c8d7ec2e8
sdttttt <760159537@qq.com>
2019-12-07 03:56:23 -0800

    Add Login module

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  get 'login' => 'users#login'
+  post 'login' => 'accessor#user_check'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_07_025727) do
+ActiveRecord::Schema.define(version: 2019_12_07_082032) do
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2019_12_07_025727) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "depscription"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
