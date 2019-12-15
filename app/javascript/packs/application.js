5d71716e09b22cd1bc217c1e7579719fe403d93d
律 <tdssssss>
2019-12-15 12:53:31 +0800

    production configuraion

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,11 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+
+import 'styles/users'
+import 'styles/products'
+import 'bootstrap/dist/js/bootstrap'
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -14,10 +19,6 @@ require("channels")
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-
-// Scss import
-import 'styles/users'
-import 'styles/products'
 
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
