59f7c4dea2fa5d6757ba8337ff268a8855d1db2b
sdttttt <760159537@qq.com>
2020-01-16 11:45:50 +0800

    update workflow

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
 class CommentsController < ApplicationController
-
   skip_before_action :before_require_login, only: [
-    :create,
+    :create
   ]
 
   def create
@@ -12,13 +12,14 @@ class CommentsController < ApplicationController
   end
 
   private
-    def comment_params
-      params.require(:comment).permit :body
-    end
 
-    def put_commenter params
-      params[:commenter] = crrent_user.username
-      params if params.key? :commenter
-    end
+  def comment_params
+    params.require(:comment).permit :body
+  end
+
+  def put_commenter(params)
+    params[:commenter] = crrent_user.username
+    params if params.key? :commenter
+  end
 
 end
