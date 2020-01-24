abe061b8570bd1a904651ac4c80869b23aa42e2d
SDTTTTT <760159537@qq.com>
2020-01-24 18:22:00 +0800

    update SQLite Install Script

--- sqlite3_31_install.sh
+++ sqlite3_31_install.sh
@@ -1,6 +1,6 @@
 
 checkSystem() {
-    return $(lsb_release -a | grep -c "$1" ) -gt 0
+    return `lsb_release -a | grep -c "$1" ` -gt 0
 }
 
 installWget() {
@@ -16,31 +16,31 @@ installWget() {
 file_name="sqlite.tar.gz"
 dir_name="sqlite-autoconf-3310000"
 
-if [ ! test -e "/bin/wget" ] 
+if ` ! test -e "/bin/wget" `
 then
     installWget
 fi
 
-if [ ! test -e "./$file_name" ]
+if ` ! test -e "./$file_name" `
 then
     wget 'https://www.sqlite.org/2020/sqlite-autoconf-3310000.tar.gz' \
     \ -c --tries=24 -O ./$file_name
 fi
 
-if [ $? -eq 0 ]
+if ` test -e "./$file_name" `
 then
 
     tar zxvf $file_name
     cd $dir_name
-    
+
     if [ -n $1 ]
     then
-        $(./configure --prefix=/usr/local)
+        `./configure --prefix=/usr/local`
     else
-        $(./configure --prefix=$1)
+        `./configure --prefix=$1`
     fi
     make && make install
-    
+
     if [ ! $? -eq 0 ]
     then
         echo 'SQLite install Failed !!'
